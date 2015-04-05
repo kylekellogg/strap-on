f213eb500d9cea90334c8a57e8d11a15af336413
Kyle Kellogg <kyle@kylekellogg.com>
2015-04-05 19:39:29 -0400

    Updated example to showcase unstrapping and restrapping, including how that affects the order of callbacks

--- example/main.lua
+++ example/main.lua
@@ -1,5 +1,4 @@
-Class = require '30log'
-StrapOn = require 'libs/strap-on'
+StrapOn = require '../strap-on'
 
 local acc1
 local acc2
@@ -10,48 +9,59 @@ local acc3
 function love.load( arg )
   --  Will be continuously called
   --  Showcases the flow of callbacks (pre, _, and post)
-  acc1 = StrapOn.strap({
-      id = 'acc1'
-    })
+  acc1 = {
+      id = 'acc1',
+      counter = 1
+    }
 
-  function acc1:preupdate( ... )
-    print( 'acc1:preupdate' )
+  acc1 = StrapOn.strap( acc1 )
+
+  function acc1.preupdate( ... )
+    print( 'acc1.preupdate' )
   end
 
-  function acc1:update( ... )
-    print( 'acc1:update' )
+  function acc1.update( ... )
+    print( 'acc1.update' )
   end
 
-  function acc1:postupdate( ... )
-    print( 'acc1:postupdate' )
+  function acc1.postupdate( ... )
+    print( 'acc1.postupdate :: ' .. acc1.counter )
+
+    if acc1.counter == 5 then
+      StrapOn.strap( acc2 )
+    end
+
+    acc1.counter = acc1.counter + 1
   end
 
   --  Will only have it's method called once
   --  Showcases how to remove from the callback flow
   --  Even though "unstrap" is called during update(), postupdate() will be called [this allows for cleanup, etc]
-  acc2 = StrapOn.strap({
+  acc2 = {
       id = 'acc2'
-    })
+    }
 
-  function acc2:update( ... )
-    print( 'acc2:update' )
+  acc2 = StrapOn.strap( acc2 )
+
+  function acc2.update( ... )
+    print( 'acc2.update' )
     acc2.unstrap()
   end
 
-  function acc2:postupdate( ... )
-    print( 'acc2:postupdate' )
+  function acc2.postupdate( ... )
+    print( 'acc2.postupdate' )
   end
 
   --  Will be continuously called
   --  Showcases selective callbacks
   acc3 = StrapOn.strap()
 
-  function acc3:update( ... )
-    print( 'acc3:update' )
+  function acc3.update( ... )
+    print( 'acc3.update' )
   end
 
-  function acc3:postupdate( ... )
-    print( 'acc3:postupdate' )
+  function acc3.postupdate( ... )
+    print( 'acc3.postupdate' )
   end
 
   --  Starting up StrapOn
@@ -60,6 +70,7 @@ end
 
 function love.update( dt )
   print( 'love.update' )
+  print( '' )
 end
 
 function love.draw()
